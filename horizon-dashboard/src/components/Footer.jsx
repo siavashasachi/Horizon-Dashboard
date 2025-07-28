d49d38bc4684872bc38a1cbca03acb9b554f59f6
siavashasachi <asachiciavash@gmail.com>
2025-07-28 09:20:26 +0330

    change footer classes base on pages

--- horizon-dashboard/src/components/Footer.jsx
+++ horizon-dashboard/src/components/Footer.jsx
@@ -1,10 +1,14 @@
+import { useLocation } from 'react-router-dom';
+const location=useLocation();
+const isLogin = location.pathname === '/login';
 function Footer() {
   return (
-    <footer className="w-full px-4 py-4 flex flex-col md:flex-row items-center justify-between text-sm text-gray-500">
+
+    <footer className={isLogin ? 'w-full px-4 py-4 flex flex-col md:flex-row items-center justify-around text-sm text-gray-500' : 'w-full px-4 py-4 flex flex-col md:flex-row items-center justify-between text-sm text-gray-500'}>
       <span>
         © {new Date().getFullYear()} Horizon UI. All Rights Reserved. Made with love by Simmmple!
       </span>
-      <div className="flex gap-4 mt-2 md:mt-0">
+      <div className= {isLogin ? "flex gap-4 mt-2 md:mt-0 text-white" : "flex gap-4 mt-2 md:mt-0"}>
         <a href="#" className="hover:underline">Marketplace</a>
         <a href="#" className="hover:underline">License</a>
         <a href="#" className="hover:underline">Terms of Use</a>
