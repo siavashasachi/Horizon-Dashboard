1379e711668e1a90170869519f59e8529fd9fd46
siavashasachi <asachiciavash@gmail.com>
2025-07-28 14:24:05 +0330

    add dark mode structure correctly

--- horizon-dashboard/tailwind.config.js
+++ horizon-dashboard/tailwind.config.js
@@ -4,10 +4,10 @@ export default {
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: 'class',
+  darkMode: 'class', // Enables class-based dark mode
   theme: {
     extend: {
-      
+      // You can optionally define default spacing, font, etc. here
     },
   },
   plugins: [],
